84ea880bb1e4a9af0b5b22285e048fb45b6b24df
Alexander Stenlund <alexander.stenlund@telia.com>
2022-07-01 11:14:31 +0200

    Cleaned up the main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,11 @@
 #include "oledDisplay.hpp"
+#include "can.h"
+#include "blink.hpp"
 
-void setup()
-{
-  Serial.begin(115200);
-  if(!oled.begin(SSD1306_SWITCHCAPVCC)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
+void setup() {
+
 }
 
-void loop(){
-  for (int i = 0; i < 11; i++) {
-    ud(&oled, speed[i], SoC[i]);
-    delay(200);
-  }
+void loop() {
+
 }
