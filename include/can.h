d0c75c5146363785d861e77ad85d1d3890317caf
Alexander Stenlund <alexander.stenlund@telia.com>
2022-07-04 14:30:58 +0200

    The ability to read can messages and its data for speed and soc

--- include/can.h
+++ include/can.h
@@ -5,6 +5,20 @@
 
 #define OWN_CAN_ID 0x37A3 // The MCU:s CAN ID
 
+enum message_id {
+    SPEED_ID = 0x423,
+    SOC_ID = 0x424,
+};
+
+struct message {
+    message_id id;
+    union {
+        uint64_t raw;
+        uint16_t speed;
+        float    soc;
+    } data;
+};
+
 /**
  * @brief Initilizes and starts the CAN controller for 500kbps in
  * normal mode on PIN 22 as TX and 23 as RX.
@@ -12,6 +26,6 @@
  * @return esp_err_t Error message from driver 
  */
 esp_err_t config_and_start_twai();
-void receive_can_message();
+void receive_can_message(message*);
 
 #endif
--- src/can.cpp
+++ src/can.cpp
@@ -1,7 +1,5 @@
 #include "can.h"
-
-// Message reception
-twai_message_t rx_message;
+#include <Arduino.h>
 
 esp_err_t config_and_start_twai() {
     twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_22, GPIO_NUM_23, TWAI_MODE_NORMAL);
@@ -20,30 +18,129 @@ esp_err_t config_and_start_twai() {
     err = twai_start();
     if (err != ESP_OK)
         return err;
+    return ESP_OK;
 }
 
-void receive_can_message() {
+/**
+ * @brief Prints the message data bytes individualy and the data 
+ * that was extracted.
+ * 
+ * @param msg This is the message the user will receive with data in 
+ * the form of a uint64_t
+ * @param rx_message This is the message that is received from the
+ *  twai_receive function and contains a data array in bytes (uint8_t)
+ */
+void print_message_debug(message* msg, twai_message_t* rx_message) {
+    Serial.print("Message: ");
+            for (int i = 0; i < 8; i++) {
+                Serial.print(" ");
+                Serial.printf("%d: %#04x", rx_message->data[i], rx_message->data[i]);
+                Serial.print(" ");
+            }
+
+            Serial.print("\n");
+        
+        Serial.print("Message: ");
+
+    Serial.printf("%#018llx: ", msg->data.raw);
+    Serial.printf("%d\n", msg->data.raw);
+
+    Serial.printf("---------------------------------------------------------------------\n");
+}
+
+// TODO: remake with a union
+
+/**
+ * @brief Converts a byte array ordered in big-endian to a uint64_t
+ * 
+ * @param arr The byte array
+ * @param length Length of the array (MAX 8)
+ * @return uint64_t The converted value
+ */
+uint64_t byte_array_b_to_uint64(uint8_t* arr, uint8_t length) {
+    uint64_t data = 0;
+
+    for(int i = 0; i < length-1; i++)
+            data = (data | arr[i]) << 8;
+    
+        data = (data | arr[7]);
+
+    return data;
+
+}
+
+// TODO: try and remake this with a union if possible.
+
+/**
+ * @brief Converts a byte array ordered in little-endian to a uint64_t
+ * 
+ * @param arr The byte array
+ * @param length Length of the array (MAX 8)
+ * @return uint64_t The converted value
+ */
+uint64_t byte_array_l_to_uint64(uint8_t* arr, uint8_t length) {
+    uint64_t data = 0;
+    for(int i = length-1; i >= 1; i--)
+            data = (data | arr[i]) << 8;
+        data = (data | arr[0]);
+    return data;
+}
+
+/**
+ * @brief Converts a uint64_t into a float.
+ * 
+ * @param i The integer that that should be converted
+ * @return float The converted uint64_t
+ */
+float uint64_to_float(uint64_t i) {
+    union {
+        uint64_t integer;
+        float floa;
+    } u;
+    u.integer = i;
+    return u.floa;
+}
+
+/**
+ * @brief Receive CAN messages from the RX queue and formats the data 
+ * into a messege struct for ease of use
+ * 
+ * @param msg The messege struct where the data should and message id be put.
+ */
+void receive_can_message(message* msg) {
+
+    // Message reception
+    twai_message_t rx_message;
+
     // Receives messages and handles them
     if (twai_receive(&rx_message, 0) == ESP_OK) {
 
-    // An example of how the CAN messages can be handled in a super loop
-    switch (rx_message.identifier)
-    {
-    // Examle case where messages with id 0x012's data is printed
-    case 0x012:
-        //Serial.printf("Message with id %#05x\n", rx_message.identifier);
-        //Serial.printf("Data received: ");
+        // Used to grab messages with specific ID's
+        // Not needed if these are the only once but here incase that 
+        // data received is in different endian types
+        switch (rx_message.identifier) {
 
-        for (int i = 0; i < rx_message.data_length_code; i++) {
-        //Serial.printf("%#04x ");
+            // If the messege received is a SPEED message
+            case SPEED_ID:
+                msg->id = SPEED_ID;
+                msg->data.raw = byte_array_l_to_uint64(
+                    rx_message.data,
+                    rx_message.data_length_code
+                );
+                break;
+
+            // If the messege received is a State of charge message
+            case SOC_ID:
+                msg->id = SOC_ID;
+                msg->data.raw = byte_array_l_to_uint64(
+                    rx_message.data,
+                    rx_message.data_length_code
+                );
+                
+                break;
+
+            default:
+                break;
         }
-
-        //Serial.printf("\n\r");
-        
-        break;
-
-    default:
-        break;
-    }
     }
 }
